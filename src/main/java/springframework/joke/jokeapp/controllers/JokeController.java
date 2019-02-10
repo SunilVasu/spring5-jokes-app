5be7ed5417b13eab35fdea91cf442d3986dd183e
SunilVasu <sunilkv24@gmail.com>
2019-02-10 14:42:52 -0600

    Completed the project - Joke App

---
+++ src/main/java/springframework/joke/jokeapp/controllers/JokeController.java
@@ -0,0 +1,26 @@
+package springframework.joke.jokeapp.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import springframework.joke.jokeapp.services.JokeService;
+
+@Controller
+public class JokeController {
+	private JokeService jokeService;
+	
+	@Autowired
+	public JokeController(JokeService jokeService) {
+		this.jokeService = jokeService;
+	}
+	
+	@RequestMapping({"/",""}) 
+	public String showJoke(Model model) {
+		
+		model.addAttribute("joke", jokeService.getJoke());
+		
+		return "chucknorris";
+	}
+}
